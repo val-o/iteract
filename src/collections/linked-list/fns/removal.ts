c9aafba9b479728ee52b94e5935dfc0932b05eee
AkhmedovValikhan <akhmedovvalikhan@gmail.com>
2019-06-05 17:07:16 +0400

    feat(linkedlist) removal fns

---
+++ src/collections/linked-list/fns/removal.ts
@@ -0,0 +1,44 @@
+import { LinkedList } from "../LinkedList";
+import { find, findLast } from "./find";
+import { LinkedListNode } from "../LinkedListNode";
+
+/**
+ * Removes first occurrence of the specified value from the linked list
+ * @param list 
+ * @param value The value to remove from the list
+ */
+export function remove<TValue>(list: LinkedList<TValue>, value: TValue): boolean {
+    const node = find(list, value);
+    if (!node) {
+        return false;
+    }
+
+    internalRemoveNode(node);
+    return true;
+}
+/**
+ * Removes last occurrence of the specified value from the linked list
+ * @param list 
+ * @param value The value to remove from the list
+ */
+export function removeLast<TValue>(list: LinkedList<TValue>, value: TValue): boolean {
+    const node = findLast(list, value);
+    if (!node) {
+        return false;
+    }
+
+    internalRemoveNode(node);
+    return true;
+}
+
+function internalRemoveNode<TValue>(node: LinkedListNode<TValue>) {
+    const prevNode = node.prev;
+    const nextNode = node.next;
+    if (prevNode) {
+        prevNode.next = nextNode;
+    }
+
+    if (nextNode) {
+        nextNode.prev = prevNode;
+    }
+}
