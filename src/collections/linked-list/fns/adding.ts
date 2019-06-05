a8799ca61ae2fdc97e31df276260134f6afb13eb
AkhmedovValikhan <akhmedovvalikhan@gmail.com>
2019-06-05 17:26:25 +0400

    feat(linkedlist) add addition fns

--- src/collections/linked-list/fns/adding.ts
+++ src/collections/linked-list/fns/adding.ts
@@ -1,19 +1,47 @@
 import { LinkedListNode } from "../LinkedListNode";
 import { LinkedList } from "../LinkedList";
 
-export function addHeadNode<TValue>(value: LinkedListNode<TValue>) {
-
+export function addHeadNode<TValue>(list: LinkedList<TValue>, node: LinkedListNode<TValue>) {
+    internalAddHeadNode(list, node);
 }
 
-export function addTailNode<TValue>(value: LinkedListNode<TValue>) {
-
+export function addTailNode<TValue>(list: LinkedList<TValue>, node: LinkedListNode<TValue>) {
+    internalAddTailNode(list, node);
 }
 
-export function addHead<TValue>(value: TValue) {
-
+export function addHead<TValue>(list: LinkedList<TValue>, value: TValue) {
+    const node = new LinkedListNode(value, list);
+    internalAddHeadNode(list, node);
 }
 
-export function addTail<TValue>(value: TValue) {
-
+export function addTail<TValue>(list: LinkedList<TValue>, value: TValue) {
+    const node = new LinkedListNode(value, list);
+    internalAddTailNode(list, node);
 }
 
+function internalAddHeadNode<TValue>(list: LinkedList<TValue>, node: LinkedListNode<TValue>) {
+    if (!list.head) {
+        // Handle empty list
+        internalAddNodeToEmptyList(list, node);
+        return;
+    }
+    list.head.prev = node;
+    node.next = list.head;
+    list.head = node;
+}
+
+function internalAddTailNode<TValue>(list: LinkedList<TValue>, node: LinkedListNode<TValue>) {
+    if (!list.head) {
+        // Handle empty list
+        internalAddNodeToEmptyList(list, node);
+        return;
+    }
+    list.tail.next = node;
+    node.prev = list.tail;
+    list.tail = node;
+}
+
+function internalAddNodeToEmptyList<TValue>(list: LinkedList<TValue>, node: LinkedListNode<TValue>) {
+    list.head = node;
+    list.tail = node;
+}
